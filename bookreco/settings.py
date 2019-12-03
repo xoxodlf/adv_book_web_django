395356a8ad674b8b27806b86e68779cf8e68027f
Taeil <xoxodlf@naver.com>
2019-12-03 18:35:02 +0900

    cc

--- bookreco/settings.py
+++ bookreco/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'book_reco',
-        'USER': 'xoxodlf',
-        'PASSWORD': 'red240188',
-        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'djemqmcor',
+        'HOST': 'adv.cot7okauvqc2.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
